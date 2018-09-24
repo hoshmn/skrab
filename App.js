ab87e76943398e4ca60944b3c8a3c5e4744981fe
Joshua Maas-Howard <joshua.maas.howard@schrodinger.com>
2018-09-23 23:50:33 -0400

    rack moved to app from board

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Board from './Board.js'
+import Rack from './Rack.js';
 
 export default class App extends React.Component {
   constructor (props) {
     super(props);
+    this.state = {
+      rack1: [{letter: 'a', value: 1},{letter: 'b', value: 1},{letter: 'c', value: 1},{letter: 'd', value: 1},{letter: 'e', value: 1},{letter: 'f', value: 1},{letter: 'g', value: 1}],
+      rack2: [{letter: 'h', value: 1},{letter: 'i', value: 1},{letter: 'j', value: 1},{letter: 'k', value: 1},{letter: 'l', value: 1},{letter: 'm', value: 1},{letter: 'n', value: 1}] };
   }
+
   render() {
     return (
       <View style={styles.container}>
+        <Text>Skrab!</Text>
         <Board />
+        <Rack rack={this.state.rack1} />
       </View>
     );
   }
--- Board.js
+++ Board.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Rack from './Rack.js';
 import Row from './Row.js';
 import buildBoard from './guts.js';
 
@@ -8,31 +7,22 @@ export default class Board extends React.Component {
   constructor (props) {
     super(props);
     const board = buildBoard();
-    this.state = {
-      board,
-      rack1: [{letter: 'a', value: 1},{letter: 'b', value: 1},{letter: 'c', value: 1},{letter: 'd', value: 1},{letter: 'e', value: 1},{letter: 'f', value: 1},{letter: 'g', value: 1}],
-      rack2: [{letter: 'h', value: 1},{letter: 'i', value: 1},{letter: 'j', value: 1},{letter: 'k', value: 1},{letter: 'l', value: 1},{letter: 'm', value: 1},{letter: 'n', value: 1}] };
+    this.state = { board };
   }
+
   render() {
     const Rows = this.state.board.map(r => 
       <Row row={r} key={r[0].row} />);
 
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.board}>
         <View style={styles.board}>{Rows}</View>
-        <Rack rack={this.state.rack1} />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   board: {
     backgroundColor: '#fff',
     alignItems: 'center',
